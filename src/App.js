84e01937eb8d1ef1e12e12d2dde2b3e4e9e9c40f
Shriyansh Chouhan <shriyanshchouhan@gmail.com>
2023-10-20 00:46:04 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState } from 'react';
 
 function App() {
+  const [city, setCity] = useState("")
+  const [search, setSearch] = useState("Mumbai")
+
+  useEffect(() => {
+
+    const fetchApi = async () => {
+      const reponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=7c0a553ee2acf61b87c35493b5cc7b7c&units=metric`);
+      const data = await reponse.json();
+      console.log(data.main);
+      setCity(data.main)
+    }
+
+    fetchApi();
+  }, [search])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="container">
+      <div className="heading">
+        [ MY WEATHER APP ]
+      </div>
+      <div className="input">
+      <input type="search" className="search" placeholder='Search city here' 
+      onChange={(e)=>{
+        setSearch(e.target.value);
+      }} 
+      value={search}   
+      />
+      </div>
+      <div>
+      <h2 className="cityName">
+        <i className="fa-solid fa-map-pin"></i>
+        {search}</h2>
+      </div>
+      {
+        !city ? <><div className="error">No data found !!</div>
+        <img src="https://previews.123rf.com/images/arcady31/arcady311705/arcady31170500009/77165345-oops-vector-banner-with-emoji.jpg" alt="" />
+        </>
+        :<div className="info">
+        <h1 className="temp">{city.temp} °C</h1>
+        <br></br>
+        <h3 className="minmax">
+          Pressure : {city.pressure} <br></br>
+          Humidity : {city.humidity} <br></br>
+          Sea Level : {city.sea_level}
+        </h3>
+      </div>
+      }
+     </div>
     </div>
   );
 }
